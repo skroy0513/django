6ab63ce8d58ce9f09a5000dee7cb1e2dfc15867a
skroy0513 <skroy0513@gmail.com>
2022-11-19 02:49:07 +0900

    제목을 표시

--- pybo/models.py
+++ pybo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# Create your models here.
 
 
 class Question(models.Model):
@@ -6,10 +7,13 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.subject
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
 
-# Create your models here.
+
